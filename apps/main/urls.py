594d641f805149ce961fbf05a66e7e333bc7e656
junaid <mjunaidttf@gmail.com>
2025-11-18 16:48:09 +0500

    Templates and data modifications

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -13,21 +13,21 @@ urlpatterns = [
     path('product_singal/', views.product_singal, name='product_singal'),
     path('our_team/', views.our_team, name='our_team'),
     path('market_sector/', views.market_sector, name='market_sector'),
+    path('textile_sector/', views.textile_sector, name='textile_sector'),
+    path('educational_trainers/', views.educational_trainers, name='educational_trainers'),
+    path('industrial_trainers/', views.industrial_trainers, name='industrial_trainers'),
+    path('testing_benches/', views.testing_benches, name='testing_benches'),
     path('our_mission/', views.our_mission, name='our_mission'),
-    path('construction_products/', views.construction_products, name='construction_products'),
-    path('aero_space_service/', views.aero_space_service, name='aero_space_service'),
-    path('railway_infrastructure/', views.railway_infrastructure, name='railway_infastracture'),
-    path('ship_building_industry/', views.ship_building_industry, name='ship_building_industry'),
-    path('power_energy/', views.power_energy, name='power_energy'),
-    path('automative_system/', views.automative_system, name='automative_system'),
+    path('cement/', views.cement, name='cement'),
+    path('fmcg/', views.fmcg, name='fmcg'),
+    path('chemicals/', views.chemicals, name='chemicals'),
     path('faqs/', views.faqs, name='faqs'),
     path("privacy-policy/", views.privacy_policy, name="privacy_policy"),
-    
-    
+
 ]
 
 # /contact us/
 # about us/
 
 # our vision
-# our mission 
+# our mission
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,53 +1,74 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def home(request):
-     return render(request, 'home/index.html' )
+    return render(request, 'home/index.html')
+
 
 def our_vision(request):
-     return render(request, 'about_us/our_vision.html' )
+    return render(request, 'about_us/our_vision.html')
+
 
 def contact_us(request):
     return render(request, 'contact_us/contact_us.html')
 
+
 def projects(request):
     return render(request, 'projects/projects.html')
+
 
 def products(request):
     return render(request, 'products/products.html')
 
+
 def product_singal(request):
     return render(request, 'products/product-single.html')
+
 
 def our_mission(request):
     return render(request, 'about_us/our_mission.html')
 
+
 def our_team(request):
     return render(request, 'about_us/our_team.html')
+
 
 def market_sector(request):
     return render(request, 'marketSectors/market-sectors.html')
 
 
-def construction_products(request):
-    return render(request, 'marketSectors/Construction-products.html')
+def textile_sector(request):
+    return render(request, 'marketSectors/textile_sector.html')
 
-def aero_space_service(request):
-    return render(request, 'marketSectors/aero-space-services.html')
 
-def railway_infrastructure(request):
-    return render(request, 'marketSectors/railway-infrastructure.html')
+def educational_trainers(request):
+    return render(request, 'marketSectors/educational_trainers.html')
 
-def ship_building_industry(request):
-    return render(request, 'marketSectors/ship-building-industry.html')
 
-def power_energy(request):
-    return render(request, 'marketSectors/power-and-energy.html')
+def industrial_trainers(request):
+    return render(request, 'marketSectors/industrial_trainers.html')
 
-def automative_system(request):
-    return render(request, 'marketSectors/automative-system.html')
+
+def testing_benches(request):
+    return render(request, 'marketSectors/testing_benches.html')
+
+
+def cement(request):
+    return render(request, 'marketSectors/cement.html')
+
+
+def fmcg(request):
+    return render(request, 'marketSectors/fmcg.html')
+
+
+def chemicals(request):
+    return render(request, 'marketSectors/chemicals.html')
+
 
 def faqs(request):
     return render(request, 'faqs/faqs.html')
+
+
 def privacy_policy(request):
     return render(request, "faqs/privacy_policy.html")
