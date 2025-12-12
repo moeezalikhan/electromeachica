3fcc56646eba61fb9cd661200ab86778880fa91f
Enter <nomannisar769@gmail.com>
2025-12-12 14:10:39 +0500

    delete unuse images and add static in .env sample.env

--- core/settings.py
+++ core/settings.py
@@ -136,10 +136,10 @@ USE_TZ = True
 # =====================================================
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.getenv('STATIC_ROOT', BASE_DIR / "staticfiles")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.getenv('MEDIA_ROOT', BASE_DIR / 'media')
 
 # =====================================================
 # EMAIL SETTINGS (loaded from .env)
