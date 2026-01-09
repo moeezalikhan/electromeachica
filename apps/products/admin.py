85d4dae5d86c281d2180f9bc87445cab99a937f6
Enter <nomannisar769@gmail.com>
2026-01-09 17:52:34 +0500

    product cart and product admin make same as projects  cart

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,32 +1,59 @@
+""" =========== Imports ============== """
+
 from django.contrib import admin
-from .models import Categories, Product, ProductImage
 from django.utils.html import format_html
+from .models import Categories, Product, ProductImage
+
 
 # ========= Categories Admin =========
 @admin.register(Categories)
 class CategoriesAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description','is_active', 'created_at', 'updated_at')
+    list_display = ('name', 'description', 'created_at', 'updated_at')
     search_fields = ('name',)
-    
 
 
 # ========= ProductImage Inline =========
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
     extra = 1
-    readonly_fields = ('image_tag',)
+    readonly_fields = ('image_preview',)
 
-    def image_tag(self, obj):
+    def image_preview(self, obj):
         if obj.image:
-            return format_html('<img src="{}" style="width: 100px; height: auto;" />', obj.image.url)
+            return format_html(
+                '<img src="{}" style="width:100px; height:auto; border-radius:6px;" />',
+                obj.image.url
+            )
         return "-"
-    image_tag.short_description = 'Image' # type: ignore
+
+    image_preview.short_description = "Image Preview" # pyright: ignore[reportFunctionMemberAccess]
+
 
 # ========= Product Admin =========
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'availability', 'created_at', 'updated_at')
+    list_display = (
+        'product_image',
+        'title',
+        'category',
+        'availability',
+        'price',
+        'created_at',
+        'updated_at',
+    )
+
     list_filter = ('availability', 'category')
-    search_fields = ('title', 'description')
-    autocomplete_fields = ['category']  # green "+" button
-    inlines = [ProductImageInline]      # show images inline in Product admin
+    search_fields = ('title', 'description', 'model_number', 'part_number')
+    autocomplete_fields = ('category',)
+    inlines = [ProductImageInline]
+
+    def product_image(self, obj):
+        first_image = obj.images.first()
+        if first_image and first_image.image:
+            return format_html(
+                '<img src="{}" style="width:60px; height:auto; border-radius:4px;" />',
+                first_image.image.url
+            )
+        return "-"
+
+    product_image.short_description = "Image" # pyright: ignore[reportFunctionMemberAccess]
